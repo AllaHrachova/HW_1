b9bb17e5cdf71711373481c202f738b91885a208
AllaHrachova <alla_krivosheina@Ukr.net>
2021-03-07 15:16:50 +0200

    version 2

--- HW_1/HW_1/Program.cs
+++ HW_1/HW_1/Program.cs
@@ -1,45 +1,58 @@
 ﻿using System;
 
-namespace ConsoleApp2
+namespace ConsoleApp5
 {
     class Program
     {
         static void Main(string[] args)
         {
             const double Pi = 3.14;
+            double r;
+            double s;
 
-            for (int i = 0; i < 3; i++)
+            int i = 0;
+            while (i < 3)
             {
-                try
+                Console.Write("Введите значение радиуса круга ->");
+                string rstring = Console.ReadLine();
+
+                Console.Write("Введите значение стороны квадрата ->");
+                string sstring = Console.ReadLine();
+
+                Double.TryParse(rstring, out r);
+                if (r != 0)
                 {
-                    Console.Write("Введите значение радиуса круга ->");
-                    double r = Convert.ToDouble(Console.ReadLine());
-
-                    Console.Write("Введите значение стороны квадрата ->");
-                    double s = Convert.ToDouble(Console.ReadLine());
-
                     double ca = Pi * r * r;
-                    double sa = s * s;
-
                     Console.WriteLine("Площадь круга равна {0}", Math.Round(ca, 2));
-                    Console.WriteLine("Площадь квадрата равна {0}", Math.Round(sa, 2));
-                    Console.ReadKey();
                 }
-                catch
+                else
                 {
-                    i = i++;
+                    i++;
                 }
+                Double.TryParse(sstring, out s);
+                if (s != 0)
+                {
+                    double sa = s * s;
+                    Console.WriteLine("Площадь квадрата равна {0}", Math.Round(sa, 2));
+                }
+                else
+                {
+                    i++;
+                }
+                if (i >= 3)
+                {
+                    Random rnd = new Random();
+                    double r1 = (rnd.NextDouble() * (5 - 0.5)) + 0.5;
+                    double s1 = (rnd.NextDouble() * (5 - 0.5)) + 0.5;
+                    double ca1 = Pi * r1 * r1;
+                    double sa1 = s1 * s1;
+
+                    Console.WriteLine("Площадь круга с рандомной стороной {0} равна {1}", Math.Round(r1, 2), Math.Round(ca1, 2));
+                    Console.WriteLine("Площадь квадрата с рандомной стороной {0} равна {1}", Math.Round(s1, 2), Math.Round(sa1, 2));
+
+                }
+                Console.ReadKey();
             }
-
-            Random rnd = new Random();
-            double r1 = (rnd.NextDouble() * (5 - 0.5)) + 0.5;
-            double s1 = (rnd.NextDouble() * (5 - 0.5)) + 0.5;
-            double ca1 = Pi * r1 * r1;
-            double sa1 = s1 * s1;
-
-            Console.WriteLine("Площадь круга с рандомной стороной {0} равна {1}", Math.Round(r1, 2), Math.Round(ca1, 2));
-            Console.WriteLine("Площадь квадрата с рандомной стороной {0} равна {1}", Math.Round(s1, 2), Math.Round(sa1, 2));
-            Console.ReadKey();
         }
     }
 }
